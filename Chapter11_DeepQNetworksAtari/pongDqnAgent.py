b826ce1db319df61464ade41b6a1986d78eec71c
Jan S <schaffranek.jan@web.de>
2021-01-15 13:25:39 +0100

    up

--- Chapter11_DeepQNetworksAtari/pongDqnAgent.py
+++ Chapter11_DeepQNetworksAtari/pongDqnAgent.py
@@ -1,3 +1,7 @@
+import tensorflow as tf
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
 import collections
 import os
 import random
