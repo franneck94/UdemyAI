9eefc05623623e5207ec37b4b05120ee97d91713
franneck94 <schaffranek.jan@web.de>
2022-01-02 10:05:08 +0100

    up

--- Chapter3_ReinforcementLearning/agentMDP.py
+++ Chapter3_ReinforcementLearning/agentMDP.py
@@ -1,9 +1,6 @@
 STATES = ["a", "b"]
 ACTIONS = ["a", "b"]
-REWARDS = {
-    "a": {"a": 0, "b": 7},
-    "b": {"a": -5, "b": 0}
-}
+REWARDS = {"a": {"a": 0, "b": 7}, "b": {"a": -5, "b": 0}}
 
 
 def main() -> None:
@@ -14,21 +11,13 @@ def main() -> None:
     print("\n\nStart-State: ", state, " Start-Reward: ", reward, "\n\n")
 
     for i in range(1, 11):
-        print("State:", state, " - Iteration: ", i)
+        print(f"State: {state} - Iteration: {i}")
         action = input("Action: ")
         if action in ACTIONS:
             reward = REWARDS[state][action]
             total_reward += reward
             state = action
-            print(
-                "Neuer State: ",
-                state,
-                " Reward: ",
-                reward,
-                " Gesamt-Reward: ",
-                total_reward,
-                "\n",
-            )
+            print(f"New State: {state} Reward: {reward} Total-Reward: {total_reward}")
             i += 1
 
 
