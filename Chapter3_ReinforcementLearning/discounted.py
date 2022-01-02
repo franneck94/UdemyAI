d4b5e855ce56741e801a4436a7238f200198a8d5
franneck94 <schaffranek.jan@web.de>
2022-01-02 11:07:16 +0100

    up

--- Chapter3_ReinforcementLearning/discounted.py
+++ Chapter3_ReinforcementLearning/discounted.py
@@ -1,26 +1,18 @@
 def discounted_reward(rewards: list, gamma: float) -> float:
-    val = 0.0
-    episode_length = len(rewards)
-    print(f"Length: {episode_length}")
-
-    for t in range(episode_length):
-        val += gamma ** (t) * rewards[t]
-        print(f"Val: {gamma ** (t) * rewards[t]}")
-
-    return val
+    result = 0.0
+    for t in range(len(rewards)):
+        result += gamma ** t * rewards[t]
+    return result
 
 
 def main() -> None:
-    gamma = 0.99
-    #         s0  s1  s2
-    #       t: 0   1   2
-    rewards = [1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
-    # 1 * gamma^0 + 0 * gamma^1 + 1 * gamma^2
-    # 1 + 0 + 0.25 = 1.25
+    gamma = 0.5  # [0, 1]
+    rewards = [1, 1, 1, -1, -1, 1, -1]
 
     discounted_reward_value = discounted_reward(rewards, gamma)
-    print(f"Rewards: {rewards}")
-    print(f"Discounted Reward: {discounted_reward_value}")
+    print(f"gamma: {gamma}")
+    print(f"rewards: {rewards}")
+    print(f"discounted_reward_value: {discounted_reward_value}")
 
 
 if __name__ == "__main__":
