16b0f67335fc29d1d4c731da38d6a39948736006
franneck94 <schaffranek.jan@web.de>
2022-01-15 11:45:26 +0100

    up

--- Chapter8_TabularQLearning/frozenLakeTabularQLearningAgent2.py
+++ Chapter8_TabularQLearning/frozenLakeTabularQLearningAgent2.py
@@ -30,19 +30,46 @@ class Agent:
         action = self.env.action_space.sample()
         return action
 
-    def get_sample(self) -> None:
+    def get_v_values(self, state: Any) -> float:
+        q_values = list(self.q_values[state].values())
+        v_value: float = np.max(q_values).astype(float)
+        return v_value
+
+    def get_sample(self) -> tuple:
+        old_state = self.state
         action = self.get_random_action()
         new_state, reward, done, _ = self.env.step(action)
         if done:
             self.state = self.env.reset()
         else:
             self.state = new_state
+        return (old_state, action, reward, new_state)
 
-    def compute_q_values(self) -> None:
-        pass
+    def compute_q_values(
+        self, state: Any, action: Any, reward: float, state_next: Any
+    ) -> None:
+        v_value_next = self.get_v_values(state_next)
+        update_q_value = reward + self.gamma * v_value_next
+        q_value_action = self.q_values[state][action]
+        new_q_value = (
+            1.0 - self.alpha
+        ) * q_value_action + self.alpha * update_q_value
+        self.q_values[state][action] = new_q_value
 
     def train(self, num_iterations: int) -> None:
-        pass
+        best_reward_mean = -np.inf
+        for iteration in range(num_iterations):
+            state, action, reward, next_state = self.get_sample()
+            self.compute_q_values(state, action, reward, next_state)
+            reward_mean = self.play(num_episodes=20, render=False)
+            if iteration % 250 == 0:
+                print(f"Iteration: {iteration}")
+            if reward_mean > best_reward_mean:
+                print(
+                    f"Old best_reward_mean: {best_reward_mean}",
+                    f"New best_reward_mean: {reward_mean}",
+                )
+                best_reward_mean = reward_mean
 
     def play(self, num_episodes: int, render: bool = True) -> float:
         env = gym.make("FrozenLake-v1")
@@ -62,7 +89,8 @@ class Agent:
                 if done:
                     reward_sum += total_reward
                     break
-            print(f"Episode: {episode} Total Reward: {total_reward}")
+            if render:
+                print(f"Episode: {episode} Total Reward: {total_reward}")
         env.close()
         return reward_sum / num_episodes
 
