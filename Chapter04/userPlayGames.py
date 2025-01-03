1ef909b3dc2b84e1aa022280eb69d9bc5c0aeb0c
franneck94 <schaffranek.jan@web.de>
2025-01-03 09:36:58 +0100

    added listing

--- Chapter04/userPlayGames.py
+++ Chapter04/userPlayGames.py
@@ -1,8 +1,14 @@
 import gym
 from gym.utils.play import play
+from gym import envs
 
 
 if __name__ == "__main__":
+    # Get a list of all installed envs (games)
+    all_envs = envs.registry.all()
+    for env in all_envs:
+        print(env)
+
     # env = gym.make("Breakout-v0")
     env = gym.make("Pong-v4")
     play(env, zoom=3)
